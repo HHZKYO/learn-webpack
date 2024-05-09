e3dda0036f117e7fe5705374b9e0df02613af0b5
HHZKYO <1692189552@qq.com>
2024-05-09 15:31:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')   //引入path
+module.exports = {
+    entry: './src/index.js',     //设置入口
+    mode: 'production',
+    output: {
+        //这里的path是一个全局变量，所以需要引入，看前面的：const path = require('require')
+        path: path.resolve(__dirname, 'dist'),  
+        filename: 'bundle.js',     //代码的意思是：把index.js打包到dist目录下，文件名为：bundle.js
+    }
+};
