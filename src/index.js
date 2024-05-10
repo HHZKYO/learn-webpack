49033d01b8b0676d36e156e7ef78acd956b60bd6
HHZKYO <1692189552@qq.com>
2024-05-10 17:47:21 +0800

    完成任务八

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import './style.css'
-import axios from 'axios';
-import * as vue from 'vue'
-import React from 'react'
-import ReactDom from 'react-dom'
-
-console.log('axios', axios)
-console.log('vue', vue)
-console.log('React', React)
-console.log('ReactDom', ReactDom)
+import png from './images/1.png'
 
 const h1 = document.createElement('h1')
+const img = document.createElement('img')
+
+img.src = png
 h1.innerText = 'hello world'
 h1.className = 'title'
 document.body.appendChild(h1)
+document.body.appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-const path = require('path')   //引入path
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    entry: './src/index.js',     //设置入口
     mode: 'development',
+    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
+            // {
+            //     test: /\.css$/i,
+            //     use: ["style-loader", "css-loader"],
+            // },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
             },
         ],
-    },
-    output: {
-        //这里的path是一个全局变量，所以需要引入，看前面的：const path = require('require')
-        path: path.resolve(__dirname, 'dist'),  
-        filename: 'bundle.js',     //代码的意思是：把index.js打包到dist目录下，文件名为：bundle.js
     }
 };
