c443159b7e2ba911b50d58e85411a2ad08abebc2
HHZKYO <1692189552@qq.com>
2024-05-10 18:11:30 +0800

    完成任务九

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ h1.innerText = 'hello world'
 h1.className = 'title'
 document.body.appendChild(h1)
 document.body.appendChild(img)
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.addEventListener('click', () => {
+    import('./a.js').then(({a}) => {
+        console.log(a)
+    }, () => {})
+})
+document.body.appendChild(button)
